9b350a6981408278e61d6d383626342585ab7064
Marissa Dilig <marissadilig@Richards-MBP.home>
2021-01-05 21:15:45 -0800

    update width height images

---
+++ tab.js
@@ -0,0 +1,15 @@
+
+        const allTabLinks = document.querySelectorAll('.tablinks');
+        const allOtherWorks = document.querySelectorAll('.otherworks-gallery');
+        let currentContentShown = 0;
+
+        for (let i = 0; i < allTabLinks.length; i++) {
+            allTabLinks[i].addEventListener('click', function () {
+                allTabLinks[currentContentShown].classList.remove('highlight');
+                allTabLinks[i].classList.add('highlight');
+                allOtherWorks[currentContentShown].classList.remove('show');
+                allOtherWorks[i].classList.add('show');
+
+                currentContentShown = i;
+            })
+        }
